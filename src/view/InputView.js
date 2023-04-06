2937f7974f518c0dbdcb3a13dcec938fb5a19c3c
cruelladevil <dev.timetravel@gmail.com>
2023-04-06 16:58:06 +0900

    refactor: InputView 상수 추출

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,22 +1,21 @@
 import Console from '../util/Console';
 
+const QUESTION = Object.freeze({
+  CAR_NAMES: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
+  RACE_STEP: '시도할 회수는 몇회인가요?',
+});
+
 const InputView = {
-  CAR_NAMES_QUESTION: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
-  RACE_STEP_QUESTION: '시도할 회수는 몇회인가요?',
-
   async readCarNames() {
-    const answer = await Console.readline(InputView.CAR_NAMES_QUESTION);
-    const whiteSpaceRegExp = /\s/g;
-    const carNames = answer.replace(whiteSpaceRegExp, '').split(',');
+    const carNames = await Console.readline(QUESTION.CAR_NAMES);
 
-    return carNames;
+    return carNames.replaceAll(/\s/g, '').split(',');
   },
 
   async readRaceStep() {
-    const answer = await Console.readline(InputView.RACE_STEP_QUESTION);
-    const raceStep = Number(answer);
+    const raceStep = await Console.readline(QUESTION.RACE_STEP);
 
-    return raceStep;
+    return Number(raceStep);
   },
 
   close() {
